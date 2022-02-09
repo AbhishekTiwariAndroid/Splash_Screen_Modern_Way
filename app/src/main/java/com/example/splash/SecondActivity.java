b08e7241bb1e1567c030b577b2c6f5349bf32d33
Abhishek <tiwariabhishekt040@gmail.com>
2022-02-09 13:34:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/splash/SecondActivity.java
@@ -0,0 +1,29 @@
+package com.example.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+        new Handler().postDelayed(new Runnable( ) {
+            @Override
+            public void run() {
+                Intent iHome = new Intent(SecondActivity.this,MainActivity.class);
+
+                startActivity(iHome);
+                finish();
+
+            }
+        },4000);
+
+    }
+}
